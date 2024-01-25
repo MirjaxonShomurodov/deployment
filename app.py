8b7e5fd3b20278d22ae94e6acc7b4d5348ed7b5a
MirjaxonShomurodov <mirjaxon457@gmail.com>
2024-01-25 16:43:23 +0500

    app.py

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-deployment
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def add():
+    return "Hello world ?"
+
+if __name__=="__main__":
+    app.run(debug=True,port=3000)
